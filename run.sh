55e50eb3ea8b7e893f6e8a588ed7e1d0f0258825
wonderit <wonsuk@broadinstitute.org>
2020-12-21 16:00:13 +0900

    edit batch-size

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 for model in resnet20 resnet32
 do
     echo "python -u trainer.py  --arch=$model  --save-dir=save_$model |& tee -a log_$model"
-    python -u trainer.py  --arch=$model  --save-dir=save_$model | tee -a log_$model
+    python -u trainer.py  --arch=$model  --batch_size=64  --save-dir=save_$model | tee -a log_$model
 done
