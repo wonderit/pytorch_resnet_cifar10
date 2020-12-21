130f30314eb90073038bb43119dd2d8e6abc96ba
wonderit <wonsuk@broadinstitute.org>
2020-12-21 12:38:07 +0900

    test run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for model in resnet20 resnet32 resnet44 resnet56 resnet110 resnet1202
+for model in resnet20 resnet32
 do
     echo "python -u trainer.py  --arch=$model  --save-dir=save_$model |& tee -a log_$model"
     python -u trainer.py  --arch=$model  --save-dir=save_$model |& tee -a log_$model
