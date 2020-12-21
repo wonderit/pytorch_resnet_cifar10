3dd7a37efc41472e76ec647854ae32752302f5fc
wonderit <wonsuk@broadinstitute.org>
2020-12-21 21:32:24 +0900

    bs 64, lr 0.01

--- run_skipinit.sh
+++ run_skipinit.sh
@@ -2,6 +2,6 @@
 
 for model in resnet20_skipinit
 do
-    echo "python -u trainer_skipinit.py  --arch=$model  --save-dir=save_$model |& tee -a log_$model"
+    echo "python -u trainer_skipinit.py  --arch=$model  --lr=0.01  --batch-size=64  --save-dir=save_$model |& tee -a log_$model"
     python -u trainer_skipinit.py  --arch=$model  --lr=0.01  --batch-size=64 --save-dir=save_$model | tee -a log_$model
 done
