8db2e297a52c896bf09fb719b0dbc2944e8853d7
wonderit <wonsuk@broadinstitute.org>
2020-12-22 12:05:15 +0900

    edit weight decay 0.001

--- run_skipinit.sh
+++ run_skipinit.sh
@@ -3,5 +3,5 @@
 for model in resnet20_skipinit
 do
     echo "python -u trainer_skipinit.py  --arch=$model  --lr=0.01  --batch-size=64  --wd=0.0005  --save-dir=save_$model |& tee -a log_$model"
-    python -u trainer_skipinit.py  --arch=$model  --lr=0.01  --batch-size=64  --wd=0.0005  --save-dir=save_$model | tee -a log_$model
+    python -u trainer_skipinit.py  --arch=$model  --lr=0.01  --batch-size=64  --wd=0.001  --save-dir=save_$model | tee -a log_$model
 done
